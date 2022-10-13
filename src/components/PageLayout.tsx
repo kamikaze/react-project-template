7793b5703b1c9f1c19117381b7d707889432bec3
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2022-10-13 16:23:20 +0300

    fixed layout

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -53,37 +53,39 @@ const PageLayout = () => {
           </Menu.Item>
         </Menu>
       </Header>
-      <Sider width={200} className='site-layout-background' collapsible={true} defaultCollapsed={true}
-             collapsedWidth={0}>
-        <Menu
-          mode='inline'
-          defaultSelectedKeys={['/school/portal/test-result-summary']}
-          defaultOpenKeys={['sub1']}
-          style={{ height: '100%', borderRight: 0 }}
-        >
-          <Menu.Item icon={<AuditOutlined />} key='/school/portal/test-result-summary'>
-            <NavLink to='/school/portal/test-result-summary'>
-              <Icon type='home' />
-              <span>{t('Test result summary')}</span>
-            </NavLink>
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout style={{ padding: '0 24px 24px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>{t('Portal')}</Breadcrumb.Item>
-          <Breadcrumb.Item>{t('Test result summary')}</Breadcrumb.Item>
-        </Breadcrumb>
-        <Content
-          className='site-layout-background'
-          style={{
-            padding: 24,
-            margin: 0,
-            minHeight: 280
-          }}
-        >
-          <Outlet />
-        </Content>
+      <Layout>
+        <Sider width={200} className='site-layout-background' collapsible={true} defaultCollapsed={true}
+               collapsedWidth={0}>
+          <Menu
+            mode='inline'
+            defaultSelectedKeys={['/school/portal/test-result-summary']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <Menu.Item icon={<AuditOutlined />} key='/school/portal/test-result-summary'>
+              <NavLink to='/school/portal/test-result-summary'>
+                <Icon type='home' />
+                <span>{t('Test result summary')}</span>
+              </NavLink>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>{t('Portal')}</Breadcrumb.Item>
+            <Breadcrumb.Item>{t('Test result summary')}</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            className='site-layout-background'
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280
+            }}
+          >
+            <Outlet />
+          </Content>
+        </Layout>
       </Layout>
     </Layout>
   )
