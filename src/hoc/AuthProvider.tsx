37a89c1d04d2440ef0cf9f5efcafe70e3b16f4fd
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2025-03-19 01:59:16 +0200

    Improved auth

--- src/hoc/AuthProvider.tsx
+++ src/hoc/AuthProvider.tsx
@@ -1,29 +1,48 @@
-import React, {createContext, useState} from "react";
+import { createContext, ReactNode, useEffect, useState } from 'react';
+import config from '../config';
 
-type AuthContextType = {
+interface AuthContextType {
   user: string | null;
-  signin: (newUser: string, cb: CallableFunction) => void;
-  signout: (cb: CallableFunction) => void;
-}
-export const AuthContext = createContext<AuthContextType>({} as AuthContextType);
-
-interface Props {
-  children: React.JSX.Element
+  loading: boolean;
+  isAuthenticated: boolean;
+  signin: (user: string, callback: VoidFunction) => void;
+  signout: (callback: VoidFunction) => void;
 }
 
-export const AuthProvider = ({children}: Props) => {
+export const AuthContext = createContext<AuthContextType>(null!);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const [user, setUser] = useState<string | null>(null);
-  const signin = (newUser: string, cb: CallableFunction) => {
-    setUser(newUser);
-    cb();
-  };
-  const signout = (cb: CallableFunction) => {
-    setUser(null);
-    cb();
-  };
-  const value = {user, signin, signout};
+  const [loading, setLoading] = useState(true);
 
-  return <AuthContext.Provider value={value}>
-    {children}
-  </AuthContext.Provider>
-}
+  // Fetch user on refresh
+  useEffect(() => {
+    fetch(config.API_BASE_URL + '/users/me', { credentials: 'include' })
+      .then((response) => response.ok ? response.json() : null)
+      .then((data) => {
+        setUser(data?.email ?? null);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  let signin = (newUser: string, callback: VoidFunction) => {
+    setUser(newUser);
+    callback();
+  };
+
+  let signout = (callback: VoidFunction) => {
+    fetch('/users/logout', {
+      method: 'POST',
+      credentials: 'include',
+    }).finally(() => {
+      setUser(null);
+      callback();
+    });
+  };
+
+  let value = { user, loading, isAuthenticated: !!user, signin, signout };
+
+  return <AuthContext.Provider value={value}>{!loading && children}</AuthContext.Provider>;
+};
